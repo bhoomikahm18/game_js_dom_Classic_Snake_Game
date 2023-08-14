a60c54426f87314c3a9c786a7696e9e94f8d0290
Bhoomika <bhoomikahm18@gmail.com>
2023-08-14 15:37:16 +0530

    Adding Arrow controls

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
 
 let gameover = false;
 let foodX, foodY;
@@ -7,6 +9,11 @@ let snakeX = 5, snakeY = 10;
 let snakeBody = [];
 let velocityX = 0, velocityY = 0;
 let setIntervalId;
+let score = 0;
+
+//Getting high score from the local storage
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
 
 const changeFoodPosition = () => {
     //Passing a random 0-30 value as food position
@@ -24,16 +31,16 @@ const handlerGameOver = () => {
 const changeDirection = (e) => {
     // console.log(e);
     //Changing the velocity value based on key press 
-    if (e.key === "ArrowUp") {
+    if (e.key === "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    } else if (e.key === "ArrowDown") {
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    } else if (e.key === "ArrowLeft") {
+    } else if (e.key === "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if (e.key === "ArrowRight") {
+    } else if (e.key === "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
@@ -49,6 +56,12 @@ const initGame = () => {
         changeFoodPosition();
         snakeBody.push([foodX, foodY]); //Pushing food position to snake body array
         // console.log(snakeBody);
+        score++; //Increase by one value
+
+        highScore = score >= highScore ? score : highScore;
+        localStorage.setItem("high-score", highScore)
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `High Score: ${highScore}`;
     }
 
     for (let i = snakeBody.length - 1; i > 0; i--) {
@@ -70,6 +83,10 @@ const initGame = () => {
     for (let i = 0; i < snakeBody.length; i++) {
         //Adding a div for each part of the snake's body.
         htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        //Checking if the snake head hit the body, if so set gameOver to true
+        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameover = true;
+        }
     }
 
     playBoard.innerHTML = htmlMarkup;
