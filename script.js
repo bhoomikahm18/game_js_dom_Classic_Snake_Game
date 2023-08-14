9b30c526620ce0c4478c4c41de7f496ad78fc992
Bhoomika <bhoomikahm18@gmail.com>
2023-08-14 14:38:49 +0530

    Working on Snake head and food

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
-// let velocityX = 0, velocityY = 0;
+let velocityX = 0, velocityY = 0;
 
 const changeFoodPosition = () => {
     //Passing a random 0-30 value as food position
@@ -11,31 +11,31 @@ const changeFoodPosition = () => {
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
-// const changeDirection = (e) => {
-//     // console.log(e);
-//     //Changing the velocity value based on key press 
-//     if(e.key === "ArrowUp") {
-//         velocityX = 0;
-//         velocityY = -1;
-//     }else if(e.key === "ArrowDown") {
-//         velocityX = 0;
-//         velocityY = 1;
-//     }else if(e.key === "ArrowLeft") {
-//         velocityX = -1;
-//         velocityY = 0;
-//     }else if(e.key === "ArrowRight") {
-//         velocityX = 1;
-//         velocityY = 0;
-//     }
-//     initGame();
-// }
+const changeDirection = (e) => {
+    console.log(e);
+    //Changing the velocity value based on key press 
+    if(e.key === "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    }else if(e.key === "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    }else if(e.key === "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    }else if(e.key === "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
+    initGame();
+}
 
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
-    //Updating the snake's head position based on the current velocity.
-    // snakeX += velocityX;
-    // snakeY += velocityY;
+    // Updating the snake's head position based on the current velocity.
+    snakeX += velocityX;
+    snakeY += velocityY;
 
     htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
@@ -43,5 +43,5 @@ const initGame = () => {
 
 changeFoodPosition();
 initGame();
-// setInterval(initGame, 125);
-// document.addEventListener("keydown", changeDirection);
+setInterval(initGame, 125);
+document.addEventListener("keydown", changeDirection);
